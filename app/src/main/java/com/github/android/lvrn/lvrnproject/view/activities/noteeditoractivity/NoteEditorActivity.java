ab2979ead097eccd245b082805f79bf4f8eed040
vboitsov <vadimboitsov1@gmail.com>
2017-05-12 18:03:36 +0300

    feat(View): Introduce commonmark-java parser of markdowns

--- app/src/main/java/com/github/android/lvrn/lvrnproject/view/activities/noteeditoractivity/NoteEditorActivity.java
+++ app/src/main/java/com/github/android/lvrn/lvrnproject/view/activities/noteeditoractivity/NoteEditorActivity.java
@@ -2,25 +2,46 @@ package com.github.android.lvrn.lvrnproject.view.activities.noteeditoractivity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 import android.widget.EditText;
 import android.widget.TabHost;
 
+import com.commonsware.cwac.anddown.AndDown;
 import com.github.android.lvrn.lvrnproject.R;
+
+import org.commonmark.Extension;
+import org.commonmark.ext.autolink.AutolinkExtension;
+import org.commonmark.ext.gfm.strikethrough.StrikethroughExtension;
+import org.commonmark.ext.gfm.tables.TablesExtension;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+import java.util.Arrays;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 
 //TODO: temporary implementation.
 public class NoteEditorActivity extends AppCompatActivity {
     private static final String EDITOR_TAB_ID = "Editor" , PREVIEW_TAB_ID = "Preview";
-    private EditText mEditorEditText;
-//    private MarkdownView mMarkdownViewPreview;
+
+    @BindView(R.id.edit_text_editor) EditText mEditorEditText;
+
+    @BindView(R.id.web_view_preview) WebView previewWebView;
+
+    private AndDown andDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_editor);
+        ButterKnife.bind(this);
         mEditorEditText = (EditText) findViewById(R.id.edit_text_editor);
-//        mMarkdownViewPreview = (MarkdownView) findViewById(R.id.markdown_view_preview);
+        andDown = new AndDown();
         initTabs();
     }
 
@@ -40,8 +61,27 @@ public class NoteEditorActivity extends AppCompatActivity {
 
     private void showPreview(String tabId) {
         if (PREVIEW_TAB_ID.equals(tabId)) {
-            CharSequence text = mEditorEditText.getText();
-//            mMarkdownViewPreview.loadMarkdown(text.toString());
+            String text = mEditorEditText.getText().toString();
+            List<Extension> extensions = Arrays.asList(
+                    TablesExtension.create(),
+                    AutolinkExtension.create(),
+                    StrikethroughExtension.create());
+
+            Parser parser = Parser.builder()
+                    .extensions(extensions)
+                    .build();
+            Node document = parser.parse(text);
+
+
+
+            HtmlRenderer renderer = HtmlRenderer.builder()
+                    .extensions(extensions)
+                    .build();
+            String textHtml = renderer.render(document);  // "<p>This is <em>Sparta</em></p>\n"
+
+            System.out.println(textHtml);
+
+            previewWebView.loadData(textHtml, "text/html", "UTF-8");
         }
     }
 }
