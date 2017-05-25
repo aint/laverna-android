49e5b331c3ff075666d00b861c130de2a7150985
vboitsov <vadimboitsov1@gmail.com>
2017-05-25 13:40:34 +0300

    fix(Persistent): Fix toEntity and toContentValues methods after introduction of Optionals in entitie

--- app/src/main/java/com/github/android/lvrn/lvrnproject/persistent/repository/extension/impl/NotebookRepositoryImpl.java
+++ app/src/main/java/com/github/android/lvrn/lvrnproject/persistent/repository/extension/impl/NotebookRepositoryImpl.java
@@ -4,10 +4,12 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.support.annotation.NonNull;
 import android.support.annotation.Size;
+import android.text.TextUtils;
 
 import com.github.android.lvrn.lvrnproject.persistent.entity.Notebook;
 import com.github.android.lvrn.lvrnproject.persistent.repository.extension.NotebookRepository;
 import com.github.android.lvrn.lvrnproject.persistent.repository.impl.ProfileDependedRepositoryImpl;
+import com.google.common.base.Optional;
 
 import java.util.List;
 
@@ -36,7 +38,7 @@ public class NotebookRepositoryImpl extends ProfileDependedRepositoryImpl<Notebo
         ContentValues contentValues = new ContentValues();
         contentValues.put(COLUMN_ID, entity.getId());
         contentValues.put(COLUMN_PROFILE_ID, entity.getProfileId());
-        contentValues.put(COLUMN_PARENT_ID, entity.getParentId());
+        contentValues.put(COLUMN_PARENT_ID, entity.getParentId().isPresent() ? entity.getParentId().get() : null);
         contentValues.put(COLUMN_NAME, entity.getName());
         contentValues.put(COLUMN_CREATION_TIME, entity.getCreationTime());
         contentValues.put(COLUMN_UPDATE_TIME, entity.getUpdateTime());
@@ -50,7 +52,8 @@ public class NotebookRepositoryImpl extends ProfileDependedRepositoryImpl<Notebo
         return new Notebook(
                 cursor.getString(cursor.getColumnIndex(COLUMN_ID)),
                 cursor.getString(cursor.getColumnIndex(COLUMN_PROFILE_ID)),
-                cursor.getString(cursor.getColumnIndex(COLUMN_PARENT_ID)),
+                !TextUtils.isEmpty(cursor.getString(cursor.getColumnIndex(COLUMN_PARENT_ID))) ?
+                        Optional.of(cursor.getString(cursor.getColumnIndex(COLUMN_PARENT_ID))) : Optional.absent(),
                 cursor.getString(cursor.getColumnIndex(COLUMN_NAME)),
                 cursor.getLong(cursor.getColumnIndex(COLUMN_CREATION_TIME)),
                 cursor.getLong(cursor.getColumnIndex(COLUMN_UPDATE_TIME)),
