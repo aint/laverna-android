1d1288fba230f2a68959532e05ad48f1d6675e37
Andrii Bei <abei@perpet.io>
2024-10-09 12:33:56 +0300

    fix(gradle) update version of libraries

--- app/src/main/java/com/github/android/lvrn/lvrnproject/view/activity/notedetail/NoteDetailActivity.kt
+++ app/src/main/java/com/github/android/lvrn/lvrnproject/view/activity/notedetail/NoteDetailActivity.kt
@@ -3,8 +3,7 @@ package com.github.android.lvrn.lvrnproject.view.activity.notedetail
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProviders
+import androidx.activity.viewModels
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.github.android.lvrn.lvrnproject.R
@@ -28,6 +27,7 @@ class NoteDetailActivity : BaseActivity() {
 
     private lateinit var binding: ActivityNoteDetailBinding
     private lateinit var navController: NavController
+    private val viewModel: NoteViewModel by viewModels { NoteViewModel.Factory }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,9 +35,8 @@ class NoteDetailActivity : BaseActivity() {
         binding = ActivityNoteDetailBinding.inflate(layoutInflater);
         setContentView(binding.root)
         val parcelableExtra = intent.getParcelableExtra<Note>("NOTE_DETAIL_KEY")
-        val noteViewModel = ViewModelProviders.of(this, NoteViewModelFactory(this)).get(NoteViewModel::class.java)
         parcelableExtra?.let {
-            noteViewModel.setNote(parcelableExtra)
+            viewModel.setNote(parcelableExtra)
             val navHostFragment = supportFragmentManager
                 .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
             navController = navHostFragment.navController
--- app/src/main/java/com/github/android/lvrn/lvrnproject/view/activity/notedetail/NoteViewModel.kt
+++ app/src/main/java/com/github/android/lvrn/lvrnproject/view/activity/notedetail/NoteViewModel.kt
@@ -3,6 +3,10 @@ package com.github.android.lvrn.lvrnproject.view.activity.notedetail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
 import com.github.valhallalabs.laverna.persistent.entity.Note
 import com.github.valhallalabs.laverna.persistent.entity.Notebook
 
@@ -21,6 +25,24 @@ class NoteViewModel : ViewModel() {
 
     fun setNotebook(notebook: Notebook) {
         this._notebook.value = notebook
+    }
+
+    companion object {
+
+        val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(
+                modelClass: Class<T>,
+                extras: CreationExtras
+            ): T {
+                // Get the Application object from extras
+                val application = checkNotNull(extras[APPLICATION_KEY])
+                // Create a SavedStateHandle for this ViewModel from extras
+                val savedStateHandle = extras.createSavedStateHandle()
+
+                return NoteViewModel() as T
+            }
+        }
     }
 
 }
